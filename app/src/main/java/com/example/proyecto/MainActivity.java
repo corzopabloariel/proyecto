60ce9a57197d9bdee1017ab9dd63eca606f09841
Pablo Corzo <pc@grupotodo.com.ar>
2020-11-15 21:40:27 -0300

    Primer

---
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.proyecto;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextName, editTextEmail, editTextPass;
+    private String name, email, pass;
+    private Button btnRegistrar;
+
+    FirebaseAuth mAuth;
+    DatabaseReference mDataBase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDataBase = FirebaseDatabase.getInstance().getReference();
+
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPass = (EditText) findViewById(R.id.editTextPass);
+
+        btnRegistrar = (Button) findViewById(R.id.btnRegistrar);
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = editTextName.getText().toString();
+                email = editTextEmail.getText().toString();
+                pass = editTextPass.getText().toString();
+
+                if (!name.isEmpty() && !email.isEmpty() && !pass.isEmpty()) {
+                    if (pass.length() >= 6) {
+                        registrarUsuario();
+                    } else {
+                        // Detalle de Firebase
+                        Toast.makeText(MainActivity.this, "La contraseña debe tener al menos 6 caracteres", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Debe completar los campos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void  registrarUsuario() {
+        mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Map<String, Object> map = new HashMap<>();
+
+                    map.put("name", name);
+                    map.put("email", email);
+                    map.put("pass", pass);
+
+                    String id = mAuth.getCurrentUser().getUid();
+                    mDataBase.child("users").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task_u) {
+                            if (task_u.isSuccessful()) {
+                                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                                finish();
+                            } else {
+                                Toast.makeText(MainActivity.this, "No se pudo crear los datos", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(MainActivity.this, "No se pudo registrar", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
