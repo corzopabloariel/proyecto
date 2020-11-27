6c7c44b7176f7f7daed8dbbd2dfe42ac907840ee
Pablo Corzo <pc@grupotodo.com.ar>
2020-11-27 20:11:25 -0300

    Cambios Servicios

---
+++ app/src/main/java/com/example/proyecto/ServiceFragment.java
@@ -0,0 +1,49 @@
+package com.example.proyecto;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ServiceFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ServiceFragment extends Fragment {
+
+    public ServiceFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment ServiceFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ServiceFragment newInstance(String param1, String param2) {
+        ServiceFragment fragment = new ServiceFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_service, container, false);
+        return view;
+    }
+}
